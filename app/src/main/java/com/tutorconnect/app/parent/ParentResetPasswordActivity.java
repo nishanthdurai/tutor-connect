03b053f6504d0ccd3b53fba05ed3b14458d1995b
Nishanth Duraisamy <nishanthduraisamy@gmail.com>
2024-08-08 21:43:27 +0530

    tutor connect password resetting for student, tutor, parent, completed

--- app/src/main/java/com/tutorconnect/app/parent/ParentResetPasswordActivity.java
+++ app/src/main/java/com/tutorconnect/app/parent/ParentResetPasswordActivity.java
@@ -1,7 +1,10 @@
 package com.tutorconnect.app.parent;
 
-import android.content.Intent;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -11,58 +14,115 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.tutorconnect.app.MailHandler;
 import com.tutorconnect.app.R;
+import com.tutorconnect.app.model.ParentTutor;
 
 public class ParentResetPasswordActivity extends AppCompatActivity {
     EditText et_sendEmail;
     Button btn_reset;
 
-    FirebaseAuth firebaseAuth;
+    DatabaseReference dbInstance;
+
+    ProgressDialog progressDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parent_reset_password);
 
-        setTitle("Parent");
+        setTitle("Parent - Forgot Password");
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Loading...");
+        progressDialog.setCancelable(false);
+
+        dbInstance = FirebaseDatabase.getInstance().getReference();
 
         // Enable the Up button
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-            getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
 
         et_sendEmail = findViewById(R.id.et_sendEmail);
         btn_reset = findViewById(R.id.btn_reset);
 
-        firebaseAuth = FirebaseAuth.getInstance();
 
         btn_reset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String email = et_sendEmail.getText().toString();
-                if (email.equals("")){
+                if (email.equals("")) {
                     Toast.makeText(ParentResetPasswordActivity.this, "Email is empty", Toast.LENGTH_SHORT).show();
-                }else{
-                    firebaseAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                } else {
+                    progressDialog.show();
+                    // check if the email present.
+                    // validate credentials with email and password
+                    Query emailQuery = dbInstance.child("parents").orderByChild("email").equalTo(email);
+                    emailQuery.addListenerForSingleValueEvent(new ValueEventListener() {
                         @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-                            if (task.isSuccessful()){
-                                Toast.makeText(ParentResetPasswordActivity.this, "Please Check Your Email", Toast.LENGTH_SHORT).show();
-                                startActivity(new Intent(ParentResetPasswordActivity.this, ParentSignIn.class));
-                            }else{
-                                String error = task.getException().getMessage();
-                                Toast.makeText(ParentResetPasswordActivity.this, error, Toast.LENGTH_SHORT).show();
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            if (dataSnapshot.exists()) {
+                                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                                    ParentTutor parent = snapshot.getValue(ParentTutor.class);
+                                    if (parent != null) {
+                                        sendEmail(email,
+                                                "Your login password",
+                                                "Your password for logging in is: " + parent.getPassword());
+                                    }
+                                }
+                            } else {
+                                progressDialog.dismiss();
+                                Toast.makeText(ParentResetPasswordActivity.this, "Email is not registered, " + "try signing up..!", Toast.LENGTH_SHORT).show();
                             }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+                            progressDialog.dismiss();
+                            Log.e("Firebase", "Database error", databaseError.toException());
+                            Toast.makeText(ParentResetPasswordActivity.this, "Error validating email, " + "try again later..!", Toast.LENGTH_SHORT).show();
                         }
                     });
                 }
             }
         });
-
     }
+
+    private void showConfirmationDialog() {
+        new AlertDialog.Builder(this)
+                .setMessage("An email has been sent to the registered email address with your password.")
+                .setCancelable(false)
+                .setPositiveButton("Got it, proceed to login", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        finish(); // Close the activity
+                    }
+                })
+                .show();
+    }
+
+    private void sendEmail(String email, String subject, String message) {
+        new Thread(() -> {
+            boolean result = MailHandler.sendEmail(email, subject, message);
+            runOnUiThread(() -> {
+                progressDialog.dismiss();
+                if (result) {
+                    showConfirmationDialog();// close the activity and go back.
+                } else {
+                    Toast.makeText(ParentResetPasswordActivity.this, "Failed to send email", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }).start();
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
--- app/src/main/java/com/tutorconnect/app/student/StudentResetPasswordActivity.java
+++ app/src/main/java/com/tutorconnect/app/student/StudentResetPasswordActivity.java
@@ -1,7 +1,10 @@
 package com.tutorconnect.app.student;
 
-import android.content.Intent;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -11,34 +14,44 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.tutorconnect.app.MailHandler;
 import com.tutorconnect.app.R;
+import com.tutorconnect.app.model.StudentTutor;
 
 public class StudentResetPasswordActivity extends AppCompatActivity {
     EditText et_sendEmail;
     Button btn_reset;
 
-    FirebaseAuth firebaseAuth;
+    DatabaseReference dbInstance;
+
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_reset_password);
 
-        setTitle("Student");
+        setTitle("Student - Reset password");
 
         // Enable the Up button
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-            getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Loading...");
+        progressDialog.setCancelable(false);
 
         et_sendEmail = findViewById(R.id.et_sendEmail);
         btn_reset = findViewById(R.id.btn_reset);
 
-        firebaseAuth = FirebaseAuth.getInstance();
+        dbInstance = FirebaseDatabase.getInstance().getReference();
 
         btn_reset.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,16 +60,33 @@ public class StudentResetPasswordActivity extends AppCompatActivity {
                 if (email.equals("")) {
                     Toast.makeText(StudentResetPasswordActivity.this, "Email is empty", Toast.LENGTH_SHORT).show();
                 } else {
-                    firebaseAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    progressDialog.show();
+                    // check if the email present.
+                    // validate credentials with email and password
+                    Query emailQuery = dbInstance.child("students").orderByChild("email").equalTo(email);
+                    emailQuery.addListenerForSingleValueEvent(new ValueEventListener() {
                         @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-                            if (task.isSuccessful()) {
-                                Toast.makeText(StudentResetPasswordActivity.this, "Please Check Your Email", Toast.LENGTH_SHORT).show();
-                                startActivity(new Intent(StudentResetPasswordActivity.this, StudentSignIn.class));
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            if (dataSnapshot.exists()) {
+                                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                                    StudentTutor student = snapshot.getValue(StudentTutor.class);
+                                    if (student != null) {
+                                        sendEmail(email,
+                                                "Your login password",
+                                                "Your password for logging in is: " + student.getPassword());
+                                    }
+                                }
                             } else {
-                                String error = task.getException().getMessage();
-                                Toast.makeText(StudentResetPasswordActivity.this, error, Toast.LENGTH_SHORT).show();
+                                progressDialog.dismiss();
+                                Toast.makeText(StudentResetPasswordActivity.this, "Email is not registered, " + "try signing up..!", Toast.LENGTH_SHORT).show();
                             }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+                            progressDialog.dismiss();
+                            Log.e("Firebase", "Database error", databaseError.toException());
+                            Toast.makeText(StudentResetPasswordActivity.this, "Error validating email, " + "try again later..!", Toast.LENGTH_SHORT).show();
                         }
                     });
                 }
@@ -64,6 +94,35 @@ public class StudentResetPasswordActivity extends AppCompatActivity {
         });
 
     }
+
+    private void showConfirmationDialog() {
+        new AlertDialog.Builder(this)
+                .setMessage("An email has been sent to the registered email address with your password.")
+                .setCancelable(false)
+                .setPositiveButton("Got it, proceed to login", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        finish(); // Close the activity
+                    }
+                })
+                .show();
+    }
+
+    private void sendEmail(String email, String subject, String message) {
+        new Thread(() -> {
+            boolean result = MailHandler.sendEmail(email, subject, message);
+            runOnUiThread(() -> {
+                progressDialog.dismiss();
+                if (result) {
+                    showConfirmationDialog();// close the activity and go back.
+                } else {
+                    Toast.makeText(StudentResetPasswordActivity.this, "Failed to send email", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }).start();
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
